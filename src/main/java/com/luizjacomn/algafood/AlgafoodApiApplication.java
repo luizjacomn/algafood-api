049cfbe4fb723dcb1dc3834880262eed7bf62909
Luiz Jacó <luizjacomn@gmail.com>
2021-04-02 17:36:43 -0300

    Initing repo

---
+++ src/main/java/com/luizjacomn/algafood/AlgafoodApiApplication.java
@@ -0,0 +1,15 @@
+package com.luizjacomn.algafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+@Controller
+public class AlgafoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodApiApplication.class, args);
+	}
+
+}
