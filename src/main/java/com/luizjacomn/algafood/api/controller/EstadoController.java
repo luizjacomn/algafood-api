f767fd74221569074a11435221005984d182b143
Luiz Jacó <luizjacomn@gmail.com>
2021-04-03 11:46:08 -0300

    Criando recurso de estado

---
+++ src/main/java/com/luizjacomn/algafood/api/controller/EstadoController.java
@@ -0,0 +1,24 @@
+package com.luizjacomn.algafood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luizjacomn.algafood.domain.model.Estado;
+import com.luizjacomn.algafood.domain.repository.EstadoRepository;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@GetMapping
+	public List<Estado> listar() {
+		return estadoRepository.findAll();
+	}
+}
