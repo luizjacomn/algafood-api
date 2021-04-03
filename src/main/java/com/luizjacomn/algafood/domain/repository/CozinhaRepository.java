fe2076980e4bb595e4db508b2af3d6576b056782
Luiz Jacó <luizjacomn@gmail.com>
2021-04-03 11:45:50 -0300

    Criando recurso de cozinha e substituindo repositório para spring data jpa

--- src/main/java/com/luizjacomn/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/luizjacomn/algafood/domain/repository/CozinhaRepository.java
@@ -1,5 +1,11 @@
 package com.luizjacomn.algafood.domain.repository;
 
-public interface CozinhaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luizjacomn.algafood.domain.model.Cozinha;
+
+@Repository
+public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
 }
