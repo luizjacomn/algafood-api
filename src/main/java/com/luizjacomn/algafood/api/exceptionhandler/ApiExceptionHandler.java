e27659e95157aa035dd41895e87e36d8b2092336
Luiz Jacó <luizjacomn@gmail.com>
2021-04-11 16:10:26 -0300

    Tratando mensagens para erros recursos não encontrados

--- src/main/java/com/luizjacomn/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/luizjacomn/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import static java.util.stream.Collectors.joining;
@@ -70,9 +71,15 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         return super.handleTypeMismatch(ex, headers, status, request);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String detail = String.format("O recurso %s, que você tentou acessar, não existe.", ex.getRequestURL());
+        return handle(ProblemType.RECURSO_NAO_ENCONTRADO, ex, detail, request);
+    }
+
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<?> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex, WebRequest request) {
-        return handle(ProblemType.ENTIDADE_NAO_ENCONTRADA, ex, request);
+        return handle(ProblemType.RECURSO_NAO_ENCONTRADO, ex, request);
     }
 
     @ExceptionHandler(EntidadeEmUsoException.class)
--- src/main/java/com/luizjacomn/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/luizjacomn/algafood/api/exceptionhandler/ProblemType.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum ProblemType {
-    ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada", 404),
+    RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado", 404),
     ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso", 409),
     ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio", 400),
     MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível", 400),
