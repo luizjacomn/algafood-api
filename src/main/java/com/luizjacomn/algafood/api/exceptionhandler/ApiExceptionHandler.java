52a6189b9ed34275142aecc7a1d2b247f1e825b8
Luiz Jacó <luizjacomn@gmail.com>
2021-04-11 14:45:46 -0300

    Tratando InvalidFormatException

--- src/main/java/com/luizjacomn/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/luizjacomn/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.luizjacomn.algafood.api.exceptionhandler;
 
+import com.fasterxml.jackson.databind.JsonMappingException.Reference;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.luizjacomn.algafood.domain.exception.EntidadeEmUsoException;
 import com.luizjacomn.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.luizjacomn.algafood.domain.exception.NegocioException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +15,24 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import static java.util.stream.Collectors.joining;
+
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Throwable rootCause = ExceptionUtils.getRootCause(ex);
+
+        if (rootCause instanceof InvalidFormatException) {
+            InvalidFormatException root = (InvalidFormatException) rootCause;
+            String path = root.getPath().stream().map(Reference::getFieldName)
+                                                    .collect(joining("."));
+            String detail = String.format("A propriedade '%s' recebeu o valor '%s', que é de um tipo inválido. " +
+                    "Corrija e informe um valor compatível com o tipo correto: %s.", path, root.getValue(), root.getTargetType().getSimpleName());
+            return handle(ProblemType.MENSAGEM_INCOMPREENSIVEL, ex, detail, request);
+        }
+
         return handle(ProblemType.MENSAGEM_INCOMPREENSIVEL, ex, "O corpo da requisição está inválido. Verifique erro de sintaxe.", request);
     }
 
