ac4d7b40b654cfcb0809b6a04047f2d13be56121
Luiz Jacó <luizjacomn@gmail.com>
2021-04-21 15:47:48 -0300

    Adicionando ModelMapper ao projeto

---
+++ src/main/java/com/luizjacomn/algafood/core/modelmapper/ModelMapperConfig.java
@@ -0,0 +1,20 @@
+package com.luizjacomn.algafood.core.modelmapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+//		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+//			.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+
+        return modelMapper;
+    }
+
+}
