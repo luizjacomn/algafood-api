1e7e8240681f6edc3b9bf4c7a662ea6b9d4ee8cc
Luiz Jacó <luizjacomn@gmail.com>
2021-04-25 10:35:53 -0300

    Renomeando endpoint de forma de pagamento

--- src/main/java/com/luizjacomn/algafood/api/controller/FormaPagamentoController.java
+++ src/main/java/com/luizjacomn/algafood/api/controller/FormaPagamentoController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/formas-pagamentos")
+@RequestMapping("/formas-pagamento")
 public class FormaPagamentoController {
 
     @Autowired
