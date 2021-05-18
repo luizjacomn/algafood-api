22239b5ca0e2e1a6d6c0d01ba5b02f18cb9bb255
Luiz Jacó <luizjacomn@gmail.com>
2021-05-18 11:25:15 -0300

    Ajustando listener de envio de e-mail para vincular à transação vigente

--- src/main/java/com/luizjacomn/algafood/domain/listener/NotificacaoPedidoConfirmadoListener.java
+++ src/main/java/com/luizjacomn/algafood/domain/listener/NotificacaoPedidoConfirmadoListener.java
@@ -4,8 +4,9 @@ import com.luizjacomn.algafood.domain.event.PedidoConfirmadoEvent;
 import com.luizjacomn.algafood.domain.model.Pedido;
 import com.luizjacomn.algafood.domain.service.mail.EnvioEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class NotificacaoPedidoConfirmadoListener {
@@ -13,7 +14,7 @@ public class NotificacaoPedidoConfirmadoListener {
     @Autowired
     private EnvioEmailService envioEmailService;
 
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
     public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
         Pedido pedido = event.getPedido();
 
