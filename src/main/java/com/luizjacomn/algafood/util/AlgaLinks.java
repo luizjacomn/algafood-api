453ab6351af815107a369efb7a5425a2dc14a18f
Luiz Jacó <luizjacomn@gmail.com>
2021-06-03 10:13:10 -0300

    Implementando o Root Entry Point da API

---
+++ src/main/java/com/luizjacomn/algafood/util/AlgaLinks.java
@@ -0,0 +1,33 @@
+package com.luizjacomn.algafood.util;
+
+import com.luizjacomn.algafood.api.controller.RestauranteController;
+import org.springframework.hateoas.Link;
+import org.springframework.stereotype.Component;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@Component
+public class AlgaLinks {
+
+    public Link linkFor(Class controllerClass, String rel) {
+        return linkTo(controllerClass).withRel(rel);
+    }
+
+    public Link linkFor(Class controllerClass) {
+        String simpleName = controllerClass.getSimpleName();
+
+        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]+");
+        Matcher matcher = pattern.matcher(simpleName);
+
+        if (matcher.find()) {
+            return linkTo(controllerClass).withRel(matcher.group().concat("s").toLowerCase());
+        } else {
+            throw new RuntimeException("Erro ao montar link para: " + simpleName);
+        }
+
+    }
+
+}
